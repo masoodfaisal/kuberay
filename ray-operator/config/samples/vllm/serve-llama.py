beee84d17b5dd76dfe8c76242737cc7d11a1ec59
Faisal Masood <masood.faisal@gmail.com>
2024-11-07 18:27:12 +1100

    Update serve-llama.py
    Signed-off-by: Faisal Masood <masood.faisal@gmail.com>

--- ray-operator/config/samples/vllm/serve-llama.py
+++ ray-operator/config/samples/vllm/serve-llama.py
@@ -1,4 +1,7 @@
-
+# https://pypi.org/project/llama-cpp-python/0.1.9/
+# https://awinml.github.io/llm-ggml-python/
+# https://github.com/abetlen/llama-cpp-python/blob/main/examples/high_level_api/langchain_custom_llm.py
+# https://github.com/huggingface/blog/blob/main/llama32.md
 
 
 import os
@@ -26,7 +29,11 @@ class LLamaCPPDeployment:
         self.n_ctx = int(os.getenv("N_CTX"))
         self.n_batch = int(os.getenv("N_BATCH"))
 
-        self.llama_cpp = Llama(model_path=MODEL_ID, n_ctx=self.n_ctx, n_batch=self.n_batch)
+        # self.llama_cpp = Llama(model_path=MODEL_ID, n_ctx=self.n_ctx, n_batch=self.n_batch)
+        self.llm = Llama.from_pretrained(
+            repo_id=model_id, #"hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF",
+            filename="*q8_0.gguf",
+)
 
     @app.post("/v1/chat/completion")
     async def call_llama(self, request: Request):
@@ -34,21 +41,31 @@ class LLamaCPPDeployment:
         print (body)
         prompt = body.get("prompt", "")
 
-        max_tokens=256
-        temperature=0.1
-        top_p=0.5
-        echo=False
-        stop=["#"]
+        # max_tokens=256
+        # temperature=0.1
+        # top_p=0.5
+        # echo=False
+        # stop=["#"]
 
-        output = self.llama_cpp(
-            prompt,
-            max_tokens=max_tokens,
-            temperature=temperature,
-            top_p=top_p,
-            echo=echo,
-            stop=stop,
-        )
-        output_text = output["choices"][0]["text"].strip()
+        # output = self.llama_cpp(
+        #     prompt,
+        #     max_tokens=max_tokens,
+        #     temperature=temperature,
+        #     top_p=top_p,
+        #     echo=echo,
+        #     stop=stop,
+        # )
+        # output_text = output["choices"][0]["text"].strip()
+        # return {"output": output_text}
+
+        output_text = llm.create_chat_completion(
+            messages = [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )        
         return {"output": output_text}
 
 
