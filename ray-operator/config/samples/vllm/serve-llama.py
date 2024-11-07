c9f8f95e894b63444b3d12f4941f67b3e26ff1ed
Faisal Masood <masood.faisal@gmail.com>
2024-11-07 22:24:08 +1100

    Update serve-llama.py
    Signed-off-by: Faisal Masood <masood.faisal@gmail.com>

--- ray-operator/config/samples/vllm/serve-llama.py
+++ ray-operator/config/samples/vllm/serve-llama.py
@@ -1,3 +1,5 @@
+
+
 import os
 import logging
 from fastapi import FastAPI
@@ -24,13 +26,13 @@ app = FastAPI()
 class LLamaCPPDeployment:
     def __init__(self):
         # Initialize the LLamaCPP model
-        self.model_id = os.getenv("MODEL_ID", default="hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF")
+        self.model_id = os.getenv("MODEL_ID", default="Qwen/Qwen2-0.5B-Instruct-GGUF")
         # self.n_ctx = int(os.getenv("N_CTX"))
         # self.n_batch = int(os.getenv("N_BATCH"))
         # self.llama_cpp = Llama(model_path=MODEL_ID, n_ctx=self.n_ctx, n_batch=self.n_batch)
-        self.llm = Llama.from_pretrained(repo_id=self.model_id, 
-                    filename="*q8_0.gguf")
+        self.llm = Llama.from_pretrained(repo_id=self.model_id,filename="*q8_0.gguf")
         #"hugging-quants/Llama-3.2-3B-Instruct-Q8_0-GGUF",
+        print("__init__ Complete")
 
     @app.post("/v1/chat/completion")
     async def call_llama(self, request: Request):
@@ -55,15 +57,21 @@ class LLamaCPPDeployment:
         # output_text = output["choices"][0]["text"].strip()
         # return {"output": output_text}
 
-        output_text = self.llm.create_chat_completion(
-            messages = [
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
+        # output_text = self.llm.create_chat_completion(
+        #     messages = [
+        #         {
+        #             "role": "user",
+        #             "content": prompt
+        #         }
+        #     ]
+        # )        
+        output = self.llm(
+            "Q: " + prompt + " A: ", # Prompt
+            max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
+            stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+            echo=True # Echo the prompt back in the output
         )        
-        return JSONResponse(content={"output": output_text})
+        return JSONResponse(content={"output": output})
 
 
 
